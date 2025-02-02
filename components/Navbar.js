eec2fe0f6e6abe8b02a6ac104d701577fab0f6e7
Sujit <kevindityam@gmail.com>
2025-02-02 18:05:44 +0700

    changed rent page

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white p-4">
+    <nav className="bg-orange-400 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <span className="text-2xl font-bold cursor-pointer">Mikey's rentals!</span>
